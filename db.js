de5a74a4bf6f54621be3b3c5e2fce6ef08d55d24
james mpanga <33938444+jimmyurl@users.noreply.github.com>
2023-09-06 22:17:32 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,10 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const User = require('./models/user');
 
-//const username = encodeURIComponent('jimmyurl');
-//const password = encodeURIComponent('Qwerty@321');
 
-const connectionString = `mongodb+srv://jimmyurl:Qwerty@321@mern-cluster.7bnyv8m.mongodb.net/mern-cluster`;
+
+const connectionString = `mongodb+srv://Admin:<password>@mern-cluster.7bnyv8m.mongodb.net/<your-database>`;
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
